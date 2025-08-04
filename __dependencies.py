98e28fd356aa9f1e288edb2a9eaf071425a302fb
Jason Kristanto <96901367+jasonkristanto188@users.noreply.github.com>
2025-08-04 20:53:19 +0700

    Add files via upload
    to run the app, use 
    streamlit run app.py

---
+++ __dependencies.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import time
+import pandas as pd 
+from datetime import datetime, timedelta, date
+from multiprocessing import Pool, freeze_support
+from functools import partial
+import streamlit as st
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.edge.options import Options
+# from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.chrome.options import Options
+
